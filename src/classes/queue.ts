48c4a21d7b93e30f7e0555f26f748b68ec77770c
karyeet <28524112+karyeet@users.noreply.github.com>
2023-12-26 18:38:57 -0800

    queue class

---
+++ src/classes/queue.ts
@@ -0,0 +1,63 @@
+export class Queue {
+  private queue: PlayableTrack[];
+  private looping = false;
+
+  private currentTrack: PlayableTrack | undefined;
+
+  public get getLength() {
+    return this.queue.length;
+  }
+
+  public get isLooping() {
+    return this.looping;
+  }
+
+  public get getCurrentTrack() {
+    return this.currentTrack;
+  }
+
+  public forceSkip() {
+    this.currentTrack = this.queue.shift();
+    return this.currentTrack;
+  }
+
+  // shift if not looping
+  public next() {
+    if (!this.looping) {
+      this.currentTrack = this.queue.shift();
+    }
+    return this.currentTrack;
+  }
+
+  constructor() {
+    this.queue = [];
+  }
+}
+
+export class PlayableTrack {
+  public readonly lavalinkEncoded: string;
+  public readonly title: string;
+  public readonly author: string;
+  public readonly artwork: string;
+  public readonly isLiveStream: boolean;
+  public readonly trackDuration: Number;
+  public readonly requester: string;
+
+  constructor(
+    encoded: string,
+    title: string,
+    author: string,
+    artwork: string,
+    isLiveStream = false,
+    trackDuration = 0,
+    requester = ''
+  ) {
+    this.lavalinkEncoded = encoded;
+    this.title = title;
+    this.author = author;
+    this.artwork = artwork;
+    this.isLiveStream = isLiveStream;
+    this.trackDuration = trackDuration;
+    this.requester = requester;
+  }
+}

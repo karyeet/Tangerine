3480495a3a05859cee86bfd10f27a1ac10ff640a
karyeet <28524112+karyeet@users.noreply.github.com>
2023-12-31 03:15:26 -0800

    rudimentary abstraction to put over lavalink clients

---
+++ src/classes/LavalinkAbstract.ts
@@ -0,0 +1,53 @@
+import type {PlayableTrack, queueItem} from './queue';
+
+export interface sources {
+  youtube: boolean;
+  soundcloud: boolean;
+  bandcamp: boolean;
+  twitch: boolean;
+  vimeo: boolean;
+  spotify: boolean;
+  deezer: boolean;
+  http: boolean;
+}
+
+export enum LoadResultType {
+  track = 'track',
+  playlist = 'playlist',
+  empty = 'empty',
+  error = 'error',
+}
+
+export interface ResolveResponse {
+  loadType: LoadResultType;
+  data: queueItem | undefined;
+}
+
+export enum PlayResponse {
+  notInVoiceChannel = 'Bot is not in a voice channel.',
+  OK = 'OK',
+}
+
+export abstract class LavalinkAbstract {
+  // object matching Lavalink REST docs https://lavalink.dev/api/rest.html#get-lavalink-info
+  abstract getInfo(): Promise<object | undefined>;
+  // array of available filter names
+  abstract getFilters(): Promise<Array<string>>;
+  // array of available sources names
+  abstract getSources(): Promise<Array<string>>;
+  // join the voice channel
+  abstract joinVoiceChannel(
+    guildid: string,
+    channelid: string,
+    shardId: number
+  ): Promise<boolean>;
+
+  // return playable track / playlist
+  abstract resolve(query: string): Promise<ResolveResponse>;
+
+  // play specified track in guild
+  abstract playTrack(
+    guildid: string,
+    track: PlayableTrack
+  ): Promise<PlayResponse>;
+}

620de4528851aba9ae0b940057be6f1016e910aa
Kareem <28524112+karyeet@users.noreply.github.com>
2024-02-19 14:35:57 -0800

    comment 'on'

--- src/classes/LavalinkAbstract.ts
+++ src/classes/LavalinkAbstract.ts
@@ -74,5 +74,6 @@ export abstract class LavalinkAbstract {
 
   abstract stopTrack(guildid: string): Promise<boolean>;
 
+  // proxy events
   abstract on(event: PlayerEvent, func: Function, guildid: string): boolean;
 }
